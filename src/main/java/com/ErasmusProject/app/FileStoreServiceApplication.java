8acdc4a71afe9c300a8a246b4254bb209d07ca27
= <stankokuveljic@gmail.com>
2017-02-13 18:40:57 +0100

    Initial commit

---
+++ src/main/java/com/ErasmusProject/app/FileStoreServiceApplication.java
@@ -0,0 +1,12 @@
+package com.ErasmusProject.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FileStoreServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FileStoreServiceApplication.class, args);
+	}
+}
