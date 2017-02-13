997552b021cdd993b3ba2bdabe53fa2347ddf956
= <stankokuveljic@gmail.com>
2017-02-13 19:42:25 +0100

    Add app conf class

--- src/main/java/com/ErasmusProject/app/FileStoreServiceApplication.java
+++ src/main/java/com/ErasmusProject/app/FileStoreServiceApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class FileStoreServiceApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(FileStoreServiceApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(FileStoreServiceApplication.class, args);
+    }
 }
