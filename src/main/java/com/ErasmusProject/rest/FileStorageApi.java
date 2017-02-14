f6355916552fe5da41081f7dbc883b7f837f48a8
= <stankokuveljic@gmail.com>
2017-02-14 17:30:34 +0100

    Implemented rest storage api

--- src/main/java/com/ErasmusProject/rest/FileStorageApi.java
+++ src/main/java/com/ErasmusProject/rest/FileStorageApi.java
@@ -1,14 +1,90 @@
 package com.ErasmusProject.rest;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.ErasmusProject.storage.PDFStorage;
+import org.apache.tomcat.util.http.fileupload.IOUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.zip.ZipOutputStream;
 
 @RestController
 @RequestMapping("/storage")
 public class FileStorageApi {
 
+    @Autowired
+    private PDFStorage storage;
+
     @RequestMapping("/ok")
     public String ok() {
         return "OK";
+    }
+
+    @RequestMapping(path = "/user", method = RequestMethod.PUT)
+    public ResponseEntity initialize(@RequestParam(name = "userid", required = true) String userid) {
+
+        storage.initialize(userid);
+        return new ResponseEntity(HttpStatus.OK);
+    }
+
+    @RequestMapping(path = "/user/pdf/{userid}", method = RequestMethod.GET)
+    public String listUserPdfs(@PathVariable(name = "userid") String userid) {
+        return "OK";
+    }
+
+    @RequestMapping(path = "/user/pdf/{userid}/zip", method = RequestMethod.GET, produces = "application/zip")
+    public void getAllZip(@PathVariable(name = "userid") String userid, HttpServletResponse response) {
+        try {
+            //setting headers
+            response.setStatus(HttpServletResponse.SC_OK);
+            response.addHeader("Content-Disposition", "attachment; filename=\"user_pdfs.zip\"");
+            ZipOutputStream zos = storage.zip(userid, response.getOutputStream());
+            zos.close();
+        } catch (IOException e) {
+
+        }
+
+    }
+
+    @RequestMapping(path = "/user/pdf/{userid}/{fileid}", method = RequestMethod.GET)
+    public void getPDF(@PathVariable(name = "userid") String userid, @PathVariable(name = "fileid") String fileid,
+            HttpServletResponse response) {
+
+        // Get your file stream from wherever.
+        InputStream myStream = null;
+        try {
+            myStream = storage.readOne(userid, fileid);
+
+            // Set the content type and attachment header.
+            response.addHeader("Content-disposition", "attachment;filename=" + fileid);
+            response.setContentType("application/pdf");
+
+            // Copy the stream to the response's output stream.
+            IOUtils.copy(myStream, response.getOutputStream());
+            response.flushBuffer();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    @RequestMapping(path = "/user/pdf", method = RequestMethod.POST, consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+    public ResponseEntity putPDF(@RequestParam(name = "userid") String userid,
+            @RequestParam(name = "file", required = true) MultipartFile file) {
+
+        try {
+            storage.write(userid, file.getOriginalFilename(), file);
+        } catch (IOException e) {
+            return new ResponseEntity(HttpStatus.EXPECTATION_FAILED);
+        }
+
+        return new ResponseEntity(HttpStatus.OK);
     }
 }
